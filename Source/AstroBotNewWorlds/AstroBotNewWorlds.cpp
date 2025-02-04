2fe36e0bdb1a0cfd33a20f129d089a05d8b9e131
JHolmes104 <jacobholmes10@icloud.com>
2025-02-04 13:16:28 +0000

    Converted To Blueprint Project

--- Source/AstroBotNewWorlds/AstroBotNewWorlds.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AstroBotNewWorlds.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AstroBotNewWorlds, "AstroBotNewWorlds" );
- 
